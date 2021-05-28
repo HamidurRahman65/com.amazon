490e7df192bdd8e0537c46c5042fb6135d99c01f
Foysol <Foysol@DESKTOP-NI0G67D>
2021-05-28 14:58:51 +0600

    First commit

---
+++ com.facebook/src/test/java/com/facebook/login/GetLoginPage.java
@@ -0,0 +1,24 @@
+package com.facebook.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GetLoginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+		driver.findElement(By.id("email")).sendKeys("Md.HamidurRahman");
+		
+	}
+
+}
